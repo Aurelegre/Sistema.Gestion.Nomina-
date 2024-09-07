f692cf7a8639634ffddd336d6987567fc8016d8c
Aurelegre <147092457+Aurelegre@users.noreply.github.com>
2024-09-06 22:22:48 -0600

    add se agregrón DTO para servicios

---
+++ Prestamos/Servicios/AutoMapperProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microsoft.Identity.Client;
+using Prestamos.Entitys;
+using Prestamos.Models;
+
+namespace Prestamos.Servicios
+{
+    public class AutoMapperProfiles : Profile
+    { 
+        public AutoMapperProfiles() 
+        {
+            CreateMap<PrestamoDTO, Prestamo>();
+
+        }
+    
+    }
+    
+}
