76e0b221683c5b921b4bbfccf2c6e7433eb9545c
Aurelegre <147092457+Aurelegre@users.noreply.github.com>
2024-10-09 00:49:05 -0600

    feat: add helper para valizar permisos y roles

---
+++ Sistema.Gestion.Nómina/Helpers/PermissionHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+public static class PermissionHelper
+{
+    // Método para verificar si el usuario tiene un permiso específico
+    public static bool HasPermission(HttpContext httpContext, string permission)
+    {
+        var userClaims = httpContext.User?.FindAll("Permission");
+        return userClaims != null && userClaims.Any(p => p.Value == permission);
+    }
+
+    // Método para verificar si el usuario tiene un rol específico
+    public static bool HasRole(HttpContext httpContext, string role)
+    {
+        var userRole = httpContext.User?.FindFirst(ClaimTypes.Role)?.Value;
+        return userRole != null && userRole == role;
+    }
+}
