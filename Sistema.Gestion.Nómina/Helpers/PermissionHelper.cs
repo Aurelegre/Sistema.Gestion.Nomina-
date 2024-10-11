bae272ed42c72e9dd4394dfa52887fbf9eba2266
Aurelegre <147092457+Aurelegre@users.noreply.github.com>
2024-10-10 22:08:48 -0600

    feat: se modificó el helper para no necesitar Httpcontext como parámetro

--- Sistema.Gestion.Nómina/Helpers/PermissionHelper.cs
+++ Sistema.Gestion.Nómina/Helpers/PermissionHelper.cs
@@ -4,17 +4,27 @@ using Microsoft.AspNetCore.Http;
 
 public static class PermissionHelper
 {
-    // Método para verificar si el usuario tiene un permiso específico
-    public static bool HasPermission(HttpContext httpContext, string permission)
+    private static IHttpContextAccessor _httpContextAccessor;
+    // Método para inicializar el HttpContextAccessor
+    public static void Configure(IHttpContextAccessor contextAccessor)
     {
-        var userClaims = httpContext.User?.FindAll("Permission");
+        _httpContextAccessor = contextAccessor;
+    }
+
+    // Método para verificar si el usuario tiene un permiso específico
+    public static bool HasPermission(string permission)
+    {
+        var httpContext = _httpContextAccessor?.HttpContext;
+        var userClaims = httpContext?.User?.FindAll("Permission");
         return userClaims != null && userClaims.Any(p => p.Value == permission);
     }
 
     // Método para verificar si el usuario tiene un rol específico
-    public static bool HasRole(HttpContext httpContext, string role)
+    public static bool HasRole(string role)
     {
-        var userRole = httpContext.User?.FindFirst(ClaimTypes.Role)?.Value;
+        var httpContext = _httpContextAccessor?.HttpContext;
+        var userRole = httpContext?.User?.FindFirst(ClaimTypes.Role)?.Value;
         return userRole != null && userRole == role;
     }
 }
+
