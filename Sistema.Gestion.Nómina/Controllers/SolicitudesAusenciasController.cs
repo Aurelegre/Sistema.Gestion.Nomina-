32494a8f7fe7b4fc24eb78c04198cc78ed91daaa
Aurelegre <147092457+Aurelegre@users.noreply.github.com>
2024-10-14 23:51:03 -0600

    feat: se terminó la autorización de solicitudes de ausencias

--- Sistema.Gestion.Nómina/Controllers/SolicitudesAusenciasController.cs
+++ Sistema.Gestion.Nómina/Controllers/SolicitudesAusenciasController.cs
@@ -5,11 +5,13 @@ using Sistema.Gestion.Nómina.DTOs.Ausencias;
 using Sistema.Gestion.Nómina.DTOs.SolicitudesAusencia;
 using Sistema.Gestion.Nómina.Entitys;
 using Sistema.Gestion.Nómina.Models;
+using Sistema.Gestion.Nómina.Services.Empresa;
 using Sistema.Gestion.Nómina.Services.Logs;
+using Sistema.Gestion.Nómina.Services.Nomina;
 
 namespace Sistema.Gestion.Nómina.Controllers
 {
-    public class SolicitudesAusenciasController (SistemaGestionNominaContext context, ILogServices logger, IMapper _mapper) : Controller
+    public class SolicitudesAusenciasController (SistemaGestionNominaContext context, ILogServices logger, INominaServices nominaServices) : Controller
     {
         [HttpGet]
         public async Task<ActionResult> Index(GetSolicitudesDTO request)
@@ -17,9 +19,10 @@ namespace Sistema.Gestion.Nómina.Controllers
             var session = logger.GetSessionData();
             try
             {
-
+                //traer el departamento del que es JEFE
                 var depto = await context.Departamentos.SingleAsync(d => d.IdJefe == session.idEmpleado);
-                var query = context.Ausencias.Where(a => a.IdEmpleadoNavigation.IdDepartamento == depto.Id && a.Autorizado == 2);
+                //traer todas las solicitudes de los departamentos menos la de el jefe
+                var query = context.Ausencias.Where(a => a.IdEmpleadoNavigation.IdDepartamento == depto.Id && a.Autorizado == 2 /*&& a.IdEmpleado != session.idEmpleado*/);
                 //aplicar filtros
                 if (!string.IsNullOrEmpty(request.Empleado))
                 {
@@ -69,7 +72,65 @@ namespace Sistema.Gestion.Nómina.Controllers
                 return View();
             }
         }
+
+        [HttpGet]
+        public async Task<ActionResult> Details (int id)
+        {
+            try
+            {
+                var solicitud = await context.Ausencias.Where(a => a.Id == id)
+                                                         .AsNoTracking()
+                                                         .Select(a => new GetSolicitudDTO
+                                                         {
+                                                             Id = a.Id,
+                                                             idEmpeado = a.IdEmpleado,
+                                                             Tipo = a.Deducible,
+                                                             Detalle = a.Detalle,
+                                                             FechaSoli = DateOnly.FromDateTime(a.FechaSolicitud),
+                                                             FechaInicio = DateOnly.FromDateTime(a.FechaInicio),
+                                                             FechaFin = DateOnly.FromDateTime(a.FechaFin),
+                                                             FechaAut = DateOnly.FromDateTime(a.FechaAutorizado.GetValueOrDefault()),
+                                                             Jefe = a.idJefeNavigation.Nombre.Substring(0, a.idJefeNavigation.Nombre.IndexOf(" ") != -1 ? a.idJefeNavigation.Nombre.IndexOf(" ") : a.idJefeNavigation.Nombre.Length) + " " + a.idJefeNavigation.Apellidos.Substring(0, a.idJefeNavigation.Apellidos.IndexOf(" ") != -1 ? a.idJefeNavigation.Apellidos.IndexOf(" ") : a.idJefeNavigation.Apellidos.Length),
+                                                             Dias = a.TotalDias,
+                                                             Estado = a.Autorizado,
+                                                             Empleado = a.IdEmpleadoNavigation.Nombre.Substring(0, a.IdEmpleadoNavigation.Nombre.IndexOf(" ") != -1 ? a.IdEmpleadoNavigation.Nombre.IndexOf(" ") : a.IdEmpleadoNavigation.Nombre.Length) + " " + a.IdEmpleadoNavigation.Apellidos.Substring(0, a.IdEmpleadoNavigation.Apellidos.IndexOf(" ") != -1 ? a.IdEmpleadoNavigation.Apellidos.IndexOf(" ") : a.IdEmpleadoNavigation.Apellidos.Length)
+                                                         })
+                                                         .FirstOrDefaultAsync();
+                if(solicitud == null)
+                {
+                    TempData["Error"] = "Error al obtener detalle de la solicitud";
+                    return RedirectToAction("Index", "SolicitudesAusencias");
+                }
+                //si el tipo es deducible, hacer el calculo
+                if(solicitud.Tipo == 1)
+                {
+                    decimal? sueldoEmpleado = await context.Empleados
+                                                    .Where(e => e.Id == solicitud.idEmpeado)
+                                                    .AsNoTracking()
+                                                    .Select(e => e.Sueldo)
+                                                    .FirstOrDefaultAsync();
+                    if (sueldoEmpleado.HasValue)
+                    {
+                        // Llamar al servicio de nómina para calcular el descuento
+                        solicitud.Deducible = nominaServices.DescuentoAusencia(sueldoEmpleado.Value, solicitud.Dias);
+                    }
+                }
+                var session = logger.GetSessionData();
+                await logger.LogTransaction(session.idEmpleado, session.company, "SolicitudesAusencias.Details", $"Se consultaron detalles de la ausencia: {id} ", session.nombre);
+
+                return Json(solicitud);
+            }
+            catch (Exception ex)
+            {
+                var session = logger.GetSessionData();
+                await logger.LogError(session.idEmpleado, session.company, "SolicitudesAusencias.Details", $"Error al consultar detalles de la ausencia: {id}", ex.Message, ex.StackTrace);
+                TempData["Error"] = "Error al consultar detalles de la Ausencia";
+                return RedirectToAction("Index", "SolicitudesAusencias");
+            }
+        }
+
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<ActionResult> Update(AuthorizeDTO request)
         {
             var session = logger.GetSessionData();
@@ -80,7 +141,7 @@ namespace Sistema.Gestion.Nómina.Controllers
                 if (ausencia == null)
                 {
                     TempData["Error"] = "La ausencia seleccionada no existe";
-                    return RedirectToAction("Index", "Ausencias");
+                    return RedirectToAction("Index", "SolicitudesAusencias");
                 }
                 ausencia.Autorizado = request.Estado;
                 ausencia.Deducible = request.Tipo;
@@ -95,13 +156,13 @@ namespace Sistema.Gestion.Nómina.Controllers
                 await logger.LogTransaction(session.idEmpleado, session.company, "SolicitudesAusencias.Update", $"Fue {accion} la ausencia del empleado id: {ausencia.IdEmpleado}, por parte de el empleado: {request.Id}", session.nombre);
 
                 TempData["Message"] = $"Ausencia {accion} con éxito";
-                return RedirectToAction("Index", "Ausencias");
+                return RedirectToAction("Index", "SolicitudesAusencias");
             }
             catch (Exception ex)
             {
                 await logger.LogError(session.idEmpleado, session.company, "SolicitudesAusencias.Update", "Error al Autorizar o denegar Solicitud de ausencia", ex.Message, ex.StackTrace);
-                TempData["Error"] = "No se pudo crear el empleado";
-                return RedirectToAction("Index", "Ausencias");
+                TempData["Error"] = "Error al modificar Solicitud";
+                return RedirectToAction("Index", "SolicitudesAusencias");
             }
         }
     }
---
+++ Sistema.Gestion.Nómina/DTOs/SolicitudesAusencia/GetSolicitudDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Sistema.Gestion.Nómina.DTOs.SolicitudesAusencia
+{
+    public class GetSolicitudDTO
+    {
+        public int Id { get; set; }
+        public int? idEmpeado { get; set; }
+        public string Empleado { get; set; }
+        public string Detalle { get; set; }
+        public DateOnly FechaSoli { get; set; }
+        public DateOnly FechaInicio { get; set; }
+        public DateOnly FechaFin { get; set; }
+        public DateOnly? FechaAut { get; set; }
+        public int? Dias { get; set; }
+        public int? Estado { get; set; }
+        public int? Tipo { get; set; }
+        public decimal? Deducible { get; set; }
+        public string? Jefe { set; get; }
+    }
+}
