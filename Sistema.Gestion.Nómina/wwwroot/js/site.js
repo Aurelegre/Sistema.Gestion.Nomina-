da46105b469860be7cb42601eea0166587640a35
Aurelegre <147092457+Aurelegre@users.noreply.github.com>
2024-10-18 01:34:06 -0600

    style: control de desplegables en el _layout

--- Sistema.Gestion.Nómina/wwwroot/js/site.js
+++ Sistema.Gestion.Nómina/wwwroot/js/site.js
@@ -15,3 +15,23 @@
         localStorage.setItem('sidebarToggled', isToggled);
     });
 });
+function closeOtherAccordions(openAccordionId) {
+    // Selecciona todos los elementos con la clase 'accordion-collapse'
+    var accordions = document.querySelectorAll('.accordion-collapse');
+
+    // Itera sobre todos los acordeones
+    accordions.forEach(function (accordion) {
+        // Si el acordeón actual no es el que tiene el ID proporcionado
+        if (accordion.id !== openAccordionId) {
+            var bsCollapse = new bootstrap.Collapse(accordion, {
+                toggle: false // No queremos que cambie de estado automáticamente
+            });
+
+            // Verifica si el acordeón está actualmente desplegado y lo colapsa
+            if (accordion.classList.contains('show')) {
+                bsCollapse.hide();
+            }
+        }
+    });
+}
+
